68cb11c9ab004ea5903d33a23788e08dbfda1026
amach02 <144416126+amach02@users.noreply.github.com>
2026-03-18 11:10:17 -0600

    SitioWebAlexander

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+const cuerpo = document.body;
+const btnAzul = document.querySelector('#btnAzul');
+const btnRojo = document.querySelector('#btnRojo');
+const btnVerde = document.querySelector('#btnVerde');
+
+
+if (btnAzul) {
+    btnAzul.addEventListener('click', () => cuerpo.style.backgroundColor = '#3498db');
+    btnRojo.addEventListener('click', () => cuerpo.style.backgroundColor = '#e74c3c');
+    btnVerde.addEventListener('click', () => cuerpo.style.backgroundColor = '#2ecc71');
+}
+
+class MenuNavegacion extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav class="navbar">
+            <div class="logo">Ejemplo Sitio Web</div>
+              <ul>
+                    <li><a href="Index.html">Inicio</a></li>
+                    <li><a href="Contacto.html">Contacto</a></li>
+                    <li><a href="https://google.com" target="_blank">Google</a></li>
+             </ul>
+        </nav>
+        `;
+    }//End connectedCallback   
+}//End class
+
+customElements.define('menu-navegacion', MenuNavegacion);
